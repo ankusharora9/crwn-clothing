cedb70d3624dc1c2b46c02f6d9f4840b09735dfe
ankusharora9 <ankusharora@live.com>
2021-03-10 16:27:30 +0530

    Latest changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
-import { Route, Switch } from "react-router-dom";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hello World !!</h1>
-  </div>
-);
+import "./App.css";
+import Header from "./components/header/header.component";
+
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
